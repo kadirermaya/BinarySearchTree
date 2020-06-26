b608cd2c3e4eaac504629acd8942051ca65fa2af
kadirermaya <kadir.ermaya@gmail.com>
2020-06-26 14:42:46 -0700

    BinarySearchTree and Node Classes created

---
+++ BinarySearchTreeProject/BinarySearchTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    public class BinarySearchTree
+    {
+        //member variables (has a)
+        //constructor (spawner)
+        //member methods (can do)
+    }
+}
